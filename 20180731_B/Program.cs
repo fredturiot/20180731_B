073e076281bb91a97ec41a51d5d330c992feed92
fredturiot <f.turiot@gmail.com>
2018-07-31 15:47:32 +0200

    1ere methode

--- 20180731_B/Program.cs
+++ 20180731_B/Program.cs
@@ -10,6 +10,31 @@ namespace _20180731_B
     {
         static void Main(string[] args)
         {
+            var test = Methode2(15);
+
+            var prenom = PoserQuestion("quel est votre prenom");
+            var nom = PoserQuestion("quel est votre nom");
+            var age = PoserQuestion("quel est votre age");
+
+            Console.WriteLine($"bonjour {prenom} {nom}, vous avez {age} ans.");
         }
+        
+        // methode d'exemple
+        static string Methode2(int age)
+        { return (age.ToString() + " ans.");}
+
+
+        static string PoserQuestion(string question)
+        {
+            Console.WriteLine(question);
+            var saisie = Console.ReadLine();
+            Console.Clear();
+
+            return saisie;
+        }
+
+        
+
+
     }
 }
